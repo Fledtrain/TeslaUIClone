036d82aff2f8e4a6ebe6c544973fc8eea3e8700e
Fledtrain <Joeyew2014@gmail.com>
2023-04-30 14:25:51 -0400

    Testing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}", "./dist/**/*.{html,js}", "./src/index.html"],
   theme: {
     extend: {},
   },
